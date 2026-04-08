6cf36a9cf7d6c2c82070a43ee329daf366bbb003
bhaskar <bhaskarraochandhaka@gmail.com>
2026-04-08 10:24:29 +0530

    shee-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,6 +5,13 @@ ami_id="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-aws ec2 run-instances --image-id $ami_id --security-group-ids $sg-009fddc01c95aa337  --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[*].{PrivateIP:PrivateIpAddress,Tags:Tags}' --output table
+instance_id=$(aws ec2 run-instances --image-id $ami_id --security-group-ids $sg-009fddc01c95aa337  --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query Instances[*]  --output table)
+if [ $instace_id == "frontend"];then
+IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
+)
+else
+IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+)
 
+fi
 done 
