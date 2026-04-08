812e9f0d655a60d099c9a28a16c05bec9eddb35c
bhaskar <bhaskarraochandhaka@gmail.com>
2026-04-08 10:44:14 +0530

    shee-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ ami_id="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-instance_id=$(aws ec2 run-instances --image-id $ami_id --security-group-ids $SG_GRP  --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query Instances[*]  --output table)
+instance_id=$(aws ec2 run-instances --image-id $ami_id --security-group-ids $SG_GRP  --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query Instances[*]  --output text)
 if [ $instance_id == "frontend" ]; then
 IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
 )
